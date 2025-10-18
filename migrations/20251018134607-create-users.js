5449cacf6c7187fba96d50c6ed508bb5a8b81bb1
Goulois Bertrand <bertrandgoulois@gmail.com>
2025-10-18 15:53:56 +0200

    feat: add models and seeders for table initialization

---
+++ migrations/20251018134607-create-users.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: { allowNull: false, primaryKey: true, type: Sequelize.UUID, defaultValue: Sequelize.literal('gen_random_uuid()') },
+      name: { type: Sequelize.STRING, allowNull: false },
+      email: { type: Sequelize.STRING, allowNull: false },
+      createdAt: { allowNull: false, type: Sequelize.DATE, defaultValue: Sequelize.literal('NOW()') },
+      updatedAt: { allowNull: false, type: Sequelize.DATE, defaultValue: Sequelize.literal('NOW()') }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ migrations/20251018134617-create-accounts.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Accounts', {
+      id: { allowNull: false, primaryKey: true, type: Sequelize.UUID, defaultValue: Sequelize.literal('gen_random_uuid()') },
+      userId: { type: Sequelize.UUID, references: { model: 'Users', key: 'id' }, allowNull: false },
+      balance: { type: Sequelize.DECIMAL(12,2), allowNull: false, defaultValue: 0 },
+      type: { type: Sequelize.STRING, allowNull: false },
+      createdAt: { allowNull: false, type: Sequelize.DATE, defaultValue: Sequelize.literal('NOW()') },
+      updatedAt: { allowNull: false, type: Sequelize.DATE, defaultValue: Sequelize.literal('NOW()') }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Accounts');
+  }
+};
