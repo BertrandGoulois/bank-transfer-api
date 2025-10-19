7d30fa7f6ee99b9bf2986b49a54d01ca29453f70
Goulois Bertrand <bertrandgoulois@gmail.com>
2025-10-19 16:43:15 +0200

    Squelette service History

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { postTransfer, postWithdrawal } = require('../controllers/accountController');
+const { postTransfer, postWithdrawal, getHistory } = require('../controllers/accountController');
 
 router.post('/:fromAccountId/transfer', postTransfer);
 router.post('/:accountId/withdraw', postWithdrawal);
+router.get('/:accountId/history', getHistory);
 
 module.exports = router;
