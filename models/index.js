2ab057b9e65d327d1b801da0808ae2612bb51488
Bertrand Goulois <bertrandgoulois@gmail.com>
2025-10-22 17:01:39 +0200

    chore: migrate configuration from config.json to .env with config.js'

--- models/index.js
+++ models/index.js
@@ -3,38 +3,60 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
+require('dotenv').config();
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// Debug: check env variables
+console.log(
+  'DB_NAME:', process.env.DB_NAME,
+  'DB_USER:', process.env.DB_USER,
+  'DB_PASSWORD:', process.env.DB_PASSWORD,
+  'DB_HOST:', process.env.DB_HOST,
+  'DB_PORT:', process.env.DB_PORT
+);
+
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+  const dbName = process.env.NODE_ENV === 'test' ? process.env.DB_NAME_TEST : process.env.DB_NAME
+
+  sequelize = new Sequelize(
+    dbName,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+      logging: false
+    }
+  );
+  console.log('Final DB:', sequelize.config.database);
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
+  .filter(file =>
+    file.indexOf('.') !== 0 &&
+    file !== basename &&
+    file.slice(-3) === '.js' &&
+    !file.endsWith('.test.js')
+  )
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+  if (db[modelName].associate) db[modelName].associate(db);
 });
 
 db.sequelize = sequelize;
